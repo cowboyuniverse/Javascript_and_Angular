0df797cb59b43a4b929e61baa55fe940d7f711e6
cowboyuniverse <cowboyuniverse@gmail.com>
2017-02-03 20:27:21 -0800

    updated and finshed up to problem 5

--- Advanced_WebDevCS4220/Week01/Week01_Assignments/Week01_HW.js
+++ Advanced_WebDevCS4220/Week01/Week01_Assignments/Week01_HW.js
@@ -16,7 +16,7 @@ reverseWord('hello')
 reverseWord('world')
 console.log()
 
-//console.log('##### 2. Write a function that takes a string and replaces all the vowels (a, e, i, o, u) with *')
+//##### 2. Write a function that takes a string and replaces all the vowels (a, e, i, o, u) with *
 function replaceVowels(string){
 	let replaceVowelString = ''
 	for(let i = 0; i < string.length; i++){
@@ -36,6 +36,43 @@ replaceVowels('angular')
 console.log()
 
 //##### 3. Write a function that finds letters in the array and outputs their occurances as an object.  The key is the letter and the count is the value. (15 pts)
+function countLetters(array){
+	let countingLetters = {}
+	let count = 0
+	for (let i = 0; i < array.length; i++){
+		for ( let j = 0; j < array.length; j++){
+			if(array[i] == array[j]){
+				count = count + 1
+				countingLetters[array[i]] = count
+			}
+		}
+		count = 0
+	}
+	console.log(countingLetters)
+}
+countLetters(['z', 'y', 'x', 'x', 'w', 'z', 'y', 'u', 'y', 'y'])
+    // { z: 2, y: 4, x: 2, w: 1, u: 1 }
+console.log()
+
+
+//##### 5. Write a function that averages all the numbers including string value numbers in an array.  (15 pts)
+function averageArray(array){
+	let sumIntValue = 0
+	let count = 0
+	for(let i = 0; i < array.length; i++){
+		if(array[i] > 0){
+			sumIntValue+= +array[i]
+			count++
+		}
+
+	}
+	let averageValue = sumIntValue/count
+
+	console.log(averageValue)
+}
+
+averageArray([3, 9, 'hello', 4, '95', 'abc', '1', 8, { key: 'value ' }])
+// 20
 
 
 
@@ -43,38 +80,18 @@ console.log()
 
 
 
+//##### 6.  Write a function that takes an array of cars and a markup percent. Return an object which contains the total markup of all cars and an array of cars with their name as the key and the value as the calculated markup.  (25 pts)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log('##### 2. Write a function that takes a string and replaces all the vowels (a, e, i, o, u) with *')
-
-
-
-
-
-
-
-
-
-
-
-// console.log()
-// //--------------------------------------------------------------------------------------------
-
+    // const cars = [
+    //   { type: 'hybrid', wholesale: 25000 },
+    //   { type: 'minivan', wholesale: 28000 },
+    //   { type: 'sedan', wholesale: 31500 },
+    //   { type: 'convertible', wholesale: 45750 }
+    // ]
+    // markupValue(cars, 10.5)
+    // { cars:
+    //    [ { hybrid: 27625 },
+    //      { minivan: 30940 },
+    //      { sedan: 34807.5 },
+    //      { convertible: 50553.75 } ],
+    //   total: 13676.25 }
